19753902ff978376dff5cd7ba683e1c8e87c6676
Michael Cassidy <mikecassidy0000@gmail.com>
2017-11-29 02:06:18 -0500

    asg

---
+++ calapp/app/assets/javascripts/components/GuineaPigs.jsx
@@ -0,0 +1,12 @@
+export class GuineaPigs extends React.Component {
+  render() {
+    let src = this.props.src;
+    return (
+      <div>
+        <h1>Cute Guinea Pigs</h1>
+        <img src={src} />
+      </div>
+    );
+  }
+}
+
